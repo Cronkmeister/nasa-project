67e458633fbe5e8be23b078263e169fd965d7432
Jonny Cronk <cronkmeister@Jonnys-MacBook-Pro.local>
2022-12-15 16:59:47 -0800

    mongoDB connection established

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,7 @@
 const http = require("http");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const app = require("./app");
 
@@ -6,9 +9,22 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = `mongodb+srv://nasa-api:${process.env.MONGO_PASSWORD}@cluster0.jfi08o9.mongodb.net/?retryWrites=true&w=majority`;
+
 const server = http.createServer(app);
 
+//mongoose event handlers
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+mongoose.set("strictQuery", false);
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
